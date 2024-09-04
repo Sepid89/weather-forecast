2f9411b167bfcd733e50e32517f700fc4e83e49c
Sepide Jamshididana <s86908@bht-berlin.de>
2024-09-04 09:23:35 +0200

    add open api

--- src/main/java/com/marketing/weather_forecast/WeatherForecastApplication.java
+++ src/main/java/com/marketing/weather_forecast/WeatherForecastApplication.java
@@ -1,11 +1,25 @@
 package com.marketing.weather_forecast;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Weather Forecast API",
+                description = "API Documentation for Weather Forecasting Service used by the Marketing Team",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Sepide Jamshididana",
+                        email = "s.jamshididana@gmail.com"
+                )
+        )
+)
 public class WeatherForecastApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/marketing/weather_forecast/controller/WeatherController.java
+++ src/main/java/com/marketing/weather_forecast/controller/WeatherController.java
@@ -2,11 +2,17 @@ package com.marketing.weather_forecast.controller;
 
 
 import com.marketing.weather_forecast.service.WeatherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "Weather Forecast Email Sender",
+        description = "Handles the scheduling and sending of weather forecast emails"
+)
 @RestController
 @RequestMapping(path = "/forecast")
 @AllArgsConstructor
@@ -14,6 +20,10 @@ public class WeatherController {
 
     private final WeatherService weatherService;
 
+    @Operation(
+            summary = "Send Weather Forecast Email",
+            description = "Triggers the sending of a weather forecast email for the next 14 days"
+    )
     @PostMapping("/send-mail-weather")
     public void scheduleWeatherEmails() {
         weatherService.sendEmail();
